d05c14a593ac6d4fd556f26085bcbaa780109ae8
Quinlan Kildare <quinlankildare@gmail.com>
2022-02-20 22:56:23 -0500

    fix PWA again again

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = withPWA({
     reactStrictMode: true,
     pwa: {
         dest: "public",
-        runtimeCaching
+        register: true,
+        skipWaiting: true,
+        runtimeCaching,
+        buildExcludes: [/middleware-manifest.json$/]
     }
 });
