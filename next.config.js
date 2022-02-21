3a036613bebf3d5e25fd315d6912fcba8cf69e53
Quinlan Kildare <quinlankildare@gmail.com>
2022-02-20 22:46:11 -0500

    fix PWA again

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
-const nextConfig = withPWA({
+const runtimeCaching = require("next-pwa/cache")
+
+module.exports = withPWA({
     reactStrictMode: true,
     pwa: {
         dest: "public",
-        register: true,
-        skipWaiting: true
+        runtimeCaching
     }
 });
-
-module.exports = nextConfig
